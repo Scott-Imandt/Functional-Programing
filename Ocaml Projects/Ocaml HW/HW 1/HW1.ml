c905bd4d70d796ae68fd0d2eb575d959b71256c6
Scott-Imandt <Scottimandt@gmail.com>
2022-03-04 23:58:51 -0500

    Completed Questions 1-10 on HW1

--- Ocaml Projects/Ocaml HW/HW 1/HW1.ml
+++ Ocaml Projects/Ocaml HW/HW 1/HW1.ml
@@ -16,7 +16,6 @@ let rec pow x n =
 print_string("Q1a: ");; print_string("3^6 -> ");; print_int(pow 3 6);;
 print_endline(" ");;
 
-
 (*Q1b*)
 let rec float_pow (x: float) n = 
   match n with
@@ -105,6 +104,148 @@ let rec slice l i j =
 print_string("Q4:  ");; printStringList(["a";"b";"c";"d";"e";"f";"g";"h"]);; print_string(" -> ");;
 printStringList(slice ["a";"b";"c";"d";"e";"f";"g";"h"] 2 6);; (* helper printStringList function from line 46*)
 print_endline(" ");;
-                  
-            
 
+(*Q5 Write a function equivs of the type ('a -> 'a -> bool) -> 'a list -> 'a list list, which partitions a list
+ into equivalence classes according to the equivalence function.*)
+                  
+
+(*Q5*)
+
+let rec listChecker list1 list2 =
+  match list1 with
+  | [] -> list2
+  | h::t -> let x = (remove_if list2 (fun x -> x = h)) in (listChecker t x);; (*Used Q3 function*)
+
+let rec equivsHelper f a lst =
+  match lst with
+  | [] -> []
+  | h::t -> if (f a h) then h :: (equivsHelper f a t) else (equivsHelper f a t);;
+
+let rec equivs f list =
+  match list with
+  | [] -> [[]]
+  | h::t -> let x = (equivsHelper f h list) in
+      let y = (listChecker x t) in
+      match y with
+      | [] -> [x]
+      | hd::tl -> x :: (equivs f y);;
+
+
+
+print_string("Q5:  ");; printIntList([1;2;3;4;5;6;7;8]);; print_string(" -> ");;
+print_string("Print output function not created");;
+print_endline(" ");;
+
+
+(*Q6 Goldbach’s conjecture states that every positive even number greater than 2 is the sum of two prime (10)
+numbers. E.g., 18 = 5 + 13, or 42 = 19 + 23. It is one of the most famous conjectures in number theory.
+It is unproven, but verified for all integers up to 4 × 1018. Write a function goldbachpair : int ->
+int * int to find two prime numbers that sum up to a given even integer. The returned pair must have
+a non-decreasing order. Note that the decomposition is not always unique. E.g., 10 can be written as 3+7 or as 5+5, so both
+(3, 7) and (5, 5) are correct answers.*)
+
+let isprime input =
+  let rec divideCheck div =
+    div * div > input || (input mod div != 0 && divideCheck (div + 1)) in
+  divideCheck 2;;
+
+let rec numcalc a b =
+  if a > b then (0,0) else if isprime a &&  isprime b then (a,b) else numcalc (a+1) (b-1);;
+
+
+let goldbachpair x =
+  match x with
+  |0 -> (0,0)
+  |1 -> (0,1)
+  |2 -> (1,1)
+  |_ -> if (x > 2) && ((x mod 2) = 0) then numcalc 1 (x-1) else (0,0);;
+
+
+let printTupleInt x =
+  match x with
+  |(a,b) -> print_string("("); print_int(a); print_string(","); print_int(b); print_string(")");;
+
+
+print_string("Q6:  ");; print_int(10);; print_string(" -> ");;
+printTupleInt(goldbachpair 10);;
+print_endline(" ");;
+
+(*Q7 Write a function called equiv on, which takes three inputs: two functions f and g, and a list lst. It (10)
+returns true if and only if the functions f and g have identical behavior on every element of lst.*)
+
+(*Q7*)
+let rec equiv_on f g list =
+  match list with
+  |[] -> true
+  |h::t -> if (f h) = (g h) then equiv_on f g t else false
+;;
+
+let x i =  i*i;;
+let z i =  3*i;;
+
+let printBoolien x =
+  match x with
+  |false -> print_string("False")
+  |true -> print_string("True")
+;;
+
+print_string("Q7:  ");; printIntList([3]);; print_string(" -> ");;
+printBoolien(equiv_on x z [3]);;
+print_endline(" ");;
+
+(*Q8 Write a functions called pairwisefilter with two parameters: (i) a function cmp that compares two
+elements of a specific type T and returns one of them, and (ii) a list lst of elements of that same type
+T. It returns a list that applies cmp while taking two items at a time from lst. If lst has odd size, the
+last element is returned “as is”.*)
+
+(*Q8*)
+let rec pairwisefilter cmp list = 
+  match list with
+  |[] -> []
+  |[_] -> list
+  |h::t -> (cmp h (List.hd t)) :: pairwisefilter cmp (List.tl t)
+;;
+
+print_string("Q8:  ");; printIntList([14; 11; 20; 25; 10; 11]);; print_string(" -> ");;
+printIntList(pairwisefilter min [14; 11; 20; 25; 10; 11]);;
+print_endline(" ");;
+
+(*Q9 Write the polynomial function, which takes a list of tuples and returns the polynomial function corresponding
+ to that list. Each tuple in the input list consists of (i) the coefficient, and (ii) the exponent.*)
+
+ let rec polynomial a = fun n ->
+  match a with
+  | [] -> 0
+  | (b, c)::tail -> let remaining = polynomial tail in
+      let x = pow n c in (*Used pow function in Q1*)
+      (b * x) + (remaining n);;
+
+let f = polynomial [3, 3; -2, 1; 5, 0];;
+
+print_string("Q9:  ");; print_string("let f = polynomial ");; print_string("[3, 3; -2, 1; 5, 0]");; print_string(" -> ");;(*Dont have proper print functrion*)
+print_int(f 2);;
+print_endline(" ");;
+
+(*Q10 The power set of a set S is the set of all subsets of S (including the empty set and the entire set).
+Write a function powerset of the type 'a list -> 'a list list, which treats lists as unordered sets,
+and returns the powerset of its input list. You may assume that the input list has no duplicates.*)
+
+(*Q10*)
+let rec powersetHelper f list =
+  match list with
+  | [] -> []
+  | h::t -> (f h) :: (powersetHelper f t);;
+
+let rec powerset list =
+  match list with
+  | [] -> [[]]
+  | h :: t ->
+      let result = powerset t in
+      result @ powersetHelper (fun s -> h :: s) result
+;;
+
+powerset [3; 4; 10];;
+
+print_string("Q10:  ");; printIntList([3; 4; 10]);; print_string(" -> ");;
+print_string("Dont have prper print return function");;
+print_endline(" ");;
