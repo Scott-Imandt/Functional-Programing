81bf89159f2c94db5a562e57a362f4d7d419deca
Scott-Imandt <scottimandt@gmail.com>
2022-04-25 19:37:03 -0400

    Created Java Functional Programing filepath and project

---
+++ Java Projects/Java HW3/HW3/src/SimpleUtils.java
@@ -0,0 +1,24 @@
+import java.util.Collection;
+
+public class SimpleUtils {
+	
+	//Q1
+	
+	/**
+	* Find and return the least element from a collection of given elements that are comparable.
+	*
+	* @param items: the given collection of elements
+	* @param from_start: a <code>boolean</code> flag that decides how ties are broken.
+	* If <code>true</code>, the element encountered earlier in the
+	* iteration is returned, otherwise the later element is returned.
+	* @param <T>: the type parameter of the collection (i.e., the items are all of type T).
+	* @return the least element in <code>items</code>, where ties are
+	* broken based on <code>from_start</code>.
+	*/
+	public static <T extends Comparable<T>> T least(Collection<T> items, boolean from_start) {
+		return null;
+	}
+	
+	
+
+}
